5c9beff7d3c153d4646b74e81efb835b4b52ae77
i5-650 <77734008+i5-650@users.noreply.github.com>
2022-03-18 16:23:12 +0100

    working db + small requests

--- back-organ-ized/routes/root.js
+++ back-organ-ized/routes/root.js
@@ -1,7 +1,38 @@
 'use strict'
+import '../database.js';
+import { Organ } from '../database.js';
+import { User } from '../database.js'; 
 
-module.exports = async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    return { root: true }
-  })
+export default  async function(fastify, opts){
+	fastify.get('/', async function (request, reply){
+		return {
+			working: true
+		}
+	});
+
+	fastify.get('/list', async function (request, reply){
+		return await Organ.findAll();
+	});
+
+	fastify.post('/organ/add', async function (request, reply){
+		if(request.body &&
+			request.body.name &&
+			request.body.price &&
+			request.body.state &&
+			request.body.age){
+				await Organ.create({
+					name: request.body.name,
+					price: request.body.price,
+					state: request.body.state,
+					age: request.body.age,
+					icon: request.body.icon
+				});
+				return {allGood: true};
+			}
+			else {
+				return {error: 'bad data'};
+			}
+	});
 }
+
+
