b5c04a6023e5e90e212a7142a9edece343b8a346
KillGuil <selin.ispir@etu.univ-lyon1.fr>
2022-03-23 17:14:58 +0100

    First Commit : Création home page : aside pour les filtres et les containers des articles

--- front-organ-ized/src/pages/Home.vue
+++ front-organ-ized/src/pages/Home.vue
@@ -1,11 +1,65 @@
 <template>
-    <div class="pictureEmbed">
-        <img src="src/assets/background.png">
-        <h1 id="titre"> Collectionneur d'organes humain</h1>
+    <div class="common-layout">
+        <el-container>
+            <el-header style="height:auto">
+                <div class="pictureEmbed">
+                    <img src="src/assets/background.png">
+                    <h1 id="titre"> Collectionneur d'organes humain</h1>
+                </div>
+            </el-header>
+            <el-container>
+                <el-aside width="200px">
+                    <div id="Categorie">
+                        <h2>Catégorie</h2>
+                        <input type="checkbox" id="cerveau" value="Cerveau" v-model="checkedCategories" />
+                        <label for="cerveau">Cerveau</label>
+                        <br/>
+                        <input type="checkbox" id="coeur" value="Coeur" v-model="checkedCategories" />
+                        <label for="coeur">Coeur</label>
+                    </div>
+                    <div id="Prix">
+                        <h2>Prix</h2>
+                        </div>
+                </el-aside>
+                <el-main>
+                    <p>Checked categories: <pre>{{ checkedCategories }}</pre></p>
+                    <el-row>
+                        <el-col v-for="(o, index) in 25"
+                            :key="o"
+                            :span="8"
+                            :offset="index > 0 ? 25 : 0"
+                        >
+                            <el-card :body-style="{ padding: '0px' }">
+                                <img src="src/assets/hearth.png" class="image"/>
+                                <div style="padding: 14%">
+                                    <el-row>
+                                        <el-col :span="19"><span>nom</span></el-col>
+                                        <el-col :span="5"><span>prix</span></el-col>
+                                    </el-row>
+                                    <div class="bottom">
+                                        <el-row>
+                                            <el-col :span="19"><span class="grid-content bg-purple-dark">Etat</span></el-col>
+                                            <el-col :span="5"><el-button type="text" class="button">Voir plus ➔ </el-button></el-col>
+                                        </el-row>
+                                    </div>
+                                </div>
+                            </el-card>
+                        </el-col>
+                    </el-row>
+                </el-main>
+            </el-container>
+        </el-container>
     </div>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      checkedCategories: ['Cerveau', 'Coeur'],
+    }
+  }
+}
 
 </script>
 
@@ -35,6 +89,10 @@
         top: 24vw;
         bottom: auto;
         font-size: 4vw;
+    }
+    .image {
+        width: 100%;
+        display: block;
     }
 
 
