77c6a6193c259e0c73bdaee4d452e0ec81932591
i5-650 <77734008+i5-650@users.noreply.github.com>
2022-03-23 17:19:15 +0100

    register + login

--- back-organ-ized/routes/root.js
+++ back-organ-ized/routes/root.js
@@ -3,8 +3,92 @@ import { Op } from 'sequelize';
 import '../database.js';
 import { Organ } from '../database.js';
 import { User } from '../database.js'; 
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { config } from 'dotenv';
+config();
 
 export default  async function(fastify, opts){
+	fastify.post('/register', async function (request, response){
+		try {
+			const {firstname, lastname, email, password} = request.body;
+
+			if(!(firstname && lastname && email && password)){
+				response.code(400).send("All input are required");
+			}
+
+			const alreadyUser = await User.findOne({where:{email: email }});
+
+			if(alreadyUser){
+				return response.code(409).send("User already exist. Please Login");
+			}
+
+			let encryptedPwd = await bcrypt.hash(password, 10);
+
+			const ohMyUser = await User.create({
+				firstname,
+				lastname,
+				email: email.toLowerCase(),
+				password: encryptedPwd
+			});
+
+			const token = jwt.sign(
+					{ user_id: ohMyUser._id, email },
+					process.env.TOKEN_KEY,
+					{
+						expiresIn: "5h",
+					}
+			)
+
+			response.code(201).send({
+				firstname,
+				lastname,
+				email : email.toLowerCase(),
+				password,
+				token
+
+			});
+		}catch(err){
+			console.log(err);
+		}
+	});
+
+	fastify.post('/login', async function (request, response){
+		try{
+			const {email, password} = request.body;
+
+			if(!(email && password)){
+				response.code(400).send("All input are required");
+			}
+
+			const user = await User.findOne({where: {email: email}});
+			
+			if(user && (await bcrypt.compare(password, user.password))){
+				const token = jwt.sign(
+					{user_id: user._id, email},
+					process.env.TOKEN_KEY,
+					{expiresIn: "5h"}
+				);
+
+				response.code(200).send(
+					{
+						firstname: user.firstname,
+						lastname: user.lastname,
+						token: token,
+						password: user.password
+					}
+				);
+			}
+			response.code(200).send("Invalid Credentials");
+		}catch(err){
+			console.log(err);
+		}
+	});
+
+	fastify.get('/users', async function (request, reply){
+		return await User.findAll();
+	});
+
 	fastify.post('/organ/add', async function (request, reply){
 		if(request.body &&
 			request.body.name &&
