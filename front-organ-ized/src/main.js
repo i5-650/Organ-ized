f61f202dbd938daca97d1c154481e90aa3e0c069
KillGuil <selin.ispir@etu.univ-lyon1.fr>
2022-03-30 16:22:25 +0200

    Filters, sideNav, ArticlesCard was added to the project

--- front-organ-ized/src/main.js
+++ front-organ-ized/src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import { router } from './routes'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(ElementPlus).mount('#app')
+
+
+
+
+
+
+
--- front-organ-ized/src/pages/Home.vue
+++ front-organ-ized/src/pages/Home.vue
@@ -7,29 +7,49 @@
                     <h1 id="titre"> Collectionneur d'organes humain</h1>
                 </div>
             </el-header>
+
             <el-container>
-                <el-aside width="200px">
+                <el-aside v-if="openAside" width="20%">
                     <div id="Categorie">
-                        <h2>Catégorie</h2>
-                        <input type="checkbox" id="cerveau" value="Cerveau" v-model="checkedCategories" />
-                        <label for="cerveau">Cerveau</label>
                         <br/>
-                        <input type="checkbox" id="coeur" value="Coeur" v-model="checkedCategories" />
-                        <label for="coeur">Coeur</label>
-                    </div>
-                    <div id="Prix">
-                        <h2>Prix</h2>
+                        <h2>Catégorie</h2>
+
+                        <br/>
+                        <div>
+                            <el-checkbox-group v-model="checkboxSelectionCategories" size="large" text-color="#TODO" fill="#TODO">
+                                <el-checkbox class="checkboxCategory" label="Vesicule biliaire" border />
+                                <el-checkbox class="checkboxCategory" label="Cerveau" round border/>
+                                <el-checkbox class="checkboxCategory" label="Poumon" round border/>
+                                <el-checkbox class="checkboxCategory" label="Foie" round border/>
+                                <el-checkbox class="checkboxCategory" label="Gros Intestin" round border/>
+                                <el-checkbox class="checkboxCategory" label="Apendicite" round border/>
+                                <el-checkbox class="checkboxCategory" label="Vessie" round border/>
+                                <el-checkbox class="checkboxCategory" label="Muscle" round border/>
+                                <el-checkbox class="checkboxCategory" label="Rein" round border/>
+                                <el-checkbox class="checkboxCategory" label="Ratte" round border/>
+                                <el-checkbox class="checkboxCategory" label="Coeur" round border/>
+                            </el-checkbox-group>
                         </div>
+                        
+                    </div>
+                    <br/>
+                    <h2>Prix (en milliers d'€) </h2>
+                    <br/>
+                    <div class="slider-demo-block">
+                        <el-slider v-model="prix" range show-stops :max="100" />
+                    </div>
+
                 </el-aside>
                 <el-main>
-                    <p>Checked categories: <pre>{{ checkedCategories }}</pre></p>
+                    <el-button @click="setOpenAside " >Nav</el-button>
+                    <!--<p>Checked categories: <pre>{{ checkedCategories }}</pre></p>-->
                     <el-row>
-                        <el-col v-for="(o, index) in 25"
+                        <el-col @click="open" v-for="(o, index) in 25"
                             :key="o"
                             :span="8"
                             :offset="index > 0 ? 25 : 0"
                         >
-                            <el-card :body-style="{ padding: '0px' }">
+                            <el-card :body-style="{ padding: '0px' }" style="margin:25px">
                                 <img src="src/assets/hearth.png" class="image"/>
                                 <div style="padding: 14%">
                                     <el-row>
@@ -52,18 +72,51 @@
     </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      checkedCategories: ['Cerveau', 'Coeur'],
-    }
-  }
+<script lang="ts" setup>
+import { ElMessage, ElMessageBox } from 'element-plus'
+import { ref } from 'vue'
+
+const openAside= ref(false)
+
+const open = () => {
+  ElMessageBox.alert('This is a message', 'Title', {
+    confirmButtonText: 'Proposer un prix',
+  })
 }
+
+const setOpenAside = () => {
+    openAside.value = !openAside.value;
+} 
+
+const checkboxSelectionCategories = ref(['Vesicule biliaire','Cerveau','Poumon','Foie','Gros Intestin','Apendicite','Vessie','Muscle','Rein','Ratte','Coeur'])
+const prix = ref([0, 100])
+
+
+
+
+
+
+
 
 </script>
 
 <style>
+
+    .checkboxCategory{
+        margin : 2%;
+    }
+
+        
+    .slider-demo-block {
+    display: flex;
+    align-items: center;
+    }
+    .slider-demo-block .el-slider {
+    margin-top: 0;
+    margin-left: 12px;
+    }
+
+
     body{
         overflow-x: hidden;
     }
