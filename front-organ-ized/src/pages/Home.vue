952e1d08fb1fb979e6c954cc2db89daf9782bdf5
lolo <l.perrin@protonmail.com>
2022-03-30 17:26:53 +0200

    re-organ-ization ;)

--- front-organ-ized/src/pages/Home.vue
+++ front-organ-ized/src/pages/Home.vue
@@ -1,74 +1,11 @@
 <template>
     <div class="common-layout">
-        <el-container>
             <el-header style="height:auto">
                 <div class="pictureEmbed">
                     <img src="src/assets/background.png">
                     <h1 id="titre"> Collectionneur d'organes humain</h1>
                 </div>
             </el-header>
-
-            <el-container>
-                <el-aside v-if="openAside" width="20%">
-                    <div id="Categorie">
-                        <br/>
-                        <h2>Catégorie</h2>
-
-                        <br/>
-                        <div>
-                            <el-checkbox-group v-model="checkboxSelectionCategories" size="large" text-color="#TODO" fill="#TODO">
-                                <el-checkbox class="checkboxCategory" label="Vesicule biliaire" border />
-                                <el-checkbox class="checkboxCategory" label="Cerveau" round border/>
-                                <el-checkbox class="checkboxCategory" label="Poumon" round border/>
-                                <el-checkbox class="checkboxCategory" label="Foie" round border/>
-                                <el-checkbox class="checkboxCategory" label="Gros Intestin" round border/>
-                                <el-checkbox class="checkboxCategory" label="Apendicite" round border/>
-                                <el-checkbox class="checkboxCategory" label="Vessie" round border/>
-                                <el-checkbox class="checkboxCategory" label="Muscle" round border/>
-                                <el-checkbox class="checkboxCategory" label="Rein" round border/>
-                                <el-checkbox class="checkboxCategory" label="Ratte" round border/>
-                                <el-checkbox class="checkboxCategory" label="Coeur" round border/>
-                            </el-checkbox-group>
-                        </div>
-                        
-                    </div>
-                    <br/>
-                    <h2>Prix (en milliers d'€) </h2>
-                    <br/>
-                    <div class="slider-demo-block">
-                        <el-slider v-model="prix" range show-stops :max="100" />
-                    </div>
-
-                </el-aside>
-                <el-main>
-                    <el-button @click="setOpenAside " >Nav</el-button>
-                    <!--<p>Checked categories: <pre>{{ checkedCategories }}</pre></p>-->
-                    <el-row>
-                        <el-col @click="open" v-for="(o, index) in 25"
-                            :key="o"
-                            :span="8"
-                            :offset="index > 0 ? 25 : 0"
-                        >
-                            <el-card :body-style="{ padding: '0px' }" style="margin:25px">
-                                <img src="src/assets/hearth.png" class="image"/>
-                                <div style="padding: 14%">
-                                    <el-row>
-                                        <el-col :span="19"><span>nom</span></el-col>
-                                        <el-col :span="5"><span>prix</span></el-col>
-                                    </el-row>
-                                    <div class="bottom">
-                                        <el-row>
-                                            <el-col :span="19"><span class="grid-content bg-purple-dark">Etat</span></el-col>
-                                            <el-col :span="5"><el-button type="text" class="button">Voir plus ➔ </el-button></el-col>
-                                        </el-row>
-                                    </div>
-                                </div>
-                            </el-card>
-                        </el-col>
-                    </el-row>
-                </el-main>
-            </el-container>
-        </el-container>
     </div>
 </template>
 
@@ -87,16 +24,6 @@ const open = () => {
 const setOpenAside = () => {
     openAside.value = !openAside.value;
 } 
-
-const checkboxSelectionCategories = ref(['Vesicule biliaire','Cerveau','Poumon','Foie','Gros Intestin','Apendicite','Vessie','Muscle','Rein','Ratte','Coeur'])
-const prix = ref([0, 100])
-
-
-
-
-
-
-
 
 </script>
 
--- front-organ-ized/src/routes/index.js
+++ front-organ-ized/src/routes/index.js
@@ -3,12 +3,14 @@ import Login from '../pages/Login.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import Account from "../pages/Account.vue";
 import Register from '../pages/Register.vue';
+import Collection from '../pages/Collection.vue';
 
 const routes = [
     {path:'/',name: 'Home', component: Home},
     {path:'/login',name: 'Login',component: Login},
     {path:'/account',name: 'Account',component: Account},
-    {path:'/register', name:'Register', component: Register}
+    {path:'/register', name:'Register', component: Register},
+	{path: '/collection', name: 'Collection', component: Collection}
 ]
 
 export const router = createRouter({
