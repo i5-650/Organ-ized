0dbbce411b4fc2eddef1f33357aec897b41f6987
i5-650 <77734008+i5-650@users.noreply.github.com>
2022-03-30 16:56:20 +0200

    register working

--- front-organ-ized/src/routes/index.js
+++ front-organ-ized/src/routes/index.js
@@ -2,11 +2,13 @@ import Home from '../pages/Home.vue'
 import Login from '../pages/Login.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import Account from "../pages/Account.vue";
+import Register from '../pages/Register.vue';
 
 const routes = [
     {path:'/',name: 'Home', component: Home},
     {path:'/login',name: 'Login',component: Login},
-    {path:'/account',name: 'Account',component: Account}
+    {path:'/account',name: 'Account',component: Account},
+    {path:'/register', name:'Register', component: Register}
 ]
 
 export const router = createRouter({
